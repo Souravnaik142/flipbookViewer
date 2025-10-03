8d9c51b9e7da378a8a0c91b1e388b47e2d50f531
Souravnaik142 <sourav.naik142@gmail.com>
2025-10-04 04:25:45 +0530

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,5 @@
+const ACCESS_KEYS = {
+  "souravnaik142@gmail.com": "abc123", // this user’s unlock key
+  "user2@example.com": "xyz789",
+  "default": "free123" // fallback for general free users
+};
