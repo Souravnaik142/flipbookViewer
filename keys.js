9c6da99dfb7492068577f813b7501d90f435b183
Souravnaik142 <sourav.naik142@gmail.com>
2025-10-04 05:30:08 +0530

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,5 +1,12 @@
-const ACCESS_KEYS = {
-  "souravnaik142@gmail.com": "abc123", // this user’s unlock key
-  "user2@example.com": "xyz789",
-  "default": "free123" // fallback for general free users
+// ✅ Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzEhgiJXph4CbXBBwxcNU3MjDCHc0rWZo",
+  authDomain: "flipbook-7540.firebaseapp.com",
+  projectId: "flipbook-7540",
+  storageBucket: "flipbook-7540.appspot.com",  // ✅ fixed
+  messagingSenderId: "430421789223",
+  appId: "1:430421789223:web:fdca22655543a637bf9c02",
+  measurementId: "G-2T9KF0DXL5"
 };
+
+export { firebaseConfig };
